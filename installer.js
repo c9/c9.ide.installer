131420de98eb77c454460c68c495065ab58eacae
Ruben Daniels <ruben@c9.io>
2013-12-11 03:58:52 +0000

    A working installation wizard that does nothing

--- installer.js
+++ installer.js
@@ -10,46 +10,19 @@ define(function(require, exports, module) {
         
         /***** Initialization *****/
         
-        var plugin = new Wizard("Ajax.org", main.consumes, {});
+        var plugin = new Wizard("Ajax.org", main.consumes, {
+            title: "Installation Wizard"
+        });
         var emit   = plugin.getEmitter();
+        
+        var logDiv, spinner, lastOutput;
         
         var loaded = false;
         function load(){
             if (loaded) return;
             loaded = true;
             
-            var div = document.body.appendChild(document.createElement("div"));
-            div.className = "installer";
-            div.innerHTML = require("text!./installer.html");
-            
-            ui.insertCss(require("text!./style.css"), options.staticPrefix, plugin);
-            
-            logDiv   = div.querySelector(".log");
-            button   = div.querySelector("button");
-            spinner  = div.querySelector(".progress");
-            
-            var cb = document.querySelector("#details");
-            cb.addEventListener("click", function(){
-                if (cb.checked) {
-                    document.querySelector(".log").className = "log details";
-                }
-                else {
-                    document.querySelector(".log").className = "log";
-                }
-            });
-            
-            plugin.addOther(function(){
-                div.innerHTML = "";
-                div.parentNode.removeChild(div);
-            });
-            
-            c9.on("stateChange", function(e){
-                if (!(e.state & c9.NETWORK)) {
-                    spinner.innerHTML = "<div style='color:orange'>Lost network "
-                        + "connection. Please restart Cloud9 IDE and "
-                        + "try again.</div>";
-                }
-            }, plugin);
+            plugin.show(true);
         }
         
         var drawn;
@@ -57,45 +30,68 @@ define(function(require, exports, module) {
             if (drawn) return;
             drawn = true;
             
+            ui.insertCss(require("text!./style.css"), plugin);
+            
             // Page Choice - explain + choice manual vs automatic
-            var choice = new WizardPage();
+            var choice = new WizardPage({ name: "choice" });
             choice.on("draw", function(options){
-                ui.insertHtml(options.container, 
+                ui.insertHtml(options.html, 
                     require("text!./pages/choice.html"), choice);
                 
             });
             
             // Page Automatic - Show Log Output & Checkbox
-            var automatic = new WizardPage();
+            var automatic = new WizardPage({ name: "automatic" });
             automatic.on("draw", function(options){
-                ui.insertHtml(options.container, 
-                    require("text!./pages/automatic.html"), automatic);
+                var div = options.html;
+                ui.insertHtml(div, require("text!./pages/automatic.html"), automatic);
                 
+                logDiv   = div.querySelector(".log");
+                spinner  = div.querySelector(".progress");
+                
+                var cb = div.querySelector("#details");
+                cb.addEventListener("click", function(){
+                    if (cb.checked) {
+                        logDiv.className = "log details";
+                    }
+                    else {
+                        logDiv.className = "log";
+                    }
+                });
+                
+                plugin.addOther(function(){
+                    div.innerHTML = "";
+                    div.parentNode.removeChild(div);
+                });
+                
+                // c9.on("stateChange", function(e){
+                //     if (!(e.state & c9.NETWORK)) {
+                //         spinner.innerHTML = "<div style='color:orange'>Lost network "
+                //             + "connection. Please restart Cloud9 IDE and "
+                //             + "try again.</div>";
+                //     }
+                // }, plugin);
+                
+                start();
             });
             
             // Page Manual - Explain the Manual Process (show terminal?) + Button to Retry
-            var manual = new WizardPage();
+            var manual = new WizardPage({ name: "manual", last: true });
             manual.on("draw", function(options){
-                ui.insertHtml(options.container, 
+                ui.insertHtml(options.html, 
                     require("text!./pages/manual.html"), manual);
                 
             });
             
-            // Page Done - Message Saying that the Installation has completed Successfully
-            var done = new WizardPage();
-            done.on("draw", function(options){
-                ui.insertHtml(options.container, 
-                    require("text!./pages/done.html"), done);
-                
+            plugin.on("next", function(e){
+                var page = e.activePage;
+                if (page.name == "choice") {
+                    var rb = page.container.querySelector("#auto");
+                    return rb.checked ? automatic : manual;
+                }
             });
             
-            // Page Error - Message Saying that the Installation has got an erro
-            var error = new WizardPage();
-            error.on("draw", function(options){
-                ui.insertHtml(options.container, 
-                    require("text!./pages/error.html"), error);
-                
-            });
+            plugin.startPage  = choice;
         }
         
         /***** Methods *****/
@@ -111,32 +107,31 @@ define(function(require, exports, module) {
         }
         
         function start(services, callback){
-            // Initialize core services
-            core.forEach(function(name){
-                if (services[name] && !services[name].name)
-                    services[name].name = name;
-            });
-            if (services.local)
-                services.local.installMode();
+            plugin.showCancel = true;
             
+            plugin.update([
+                { id: "previous", visible: false },
+                { id: "next", visible: false }
+            ]);
+            
+            // Start Installation
+            logln("Starting Installation...");
+            
+            // @fjakobs push errors to this array if any
             var errors = [];
-            var i = 0, keys = Object.keys(services);
             
-            function next(err){
-                if (err) { 
-                    errors.push(err);
-                    logln("<div class='error'>" + (err.message || err) + "</div>");
-                    return done();
-                }
-                
-                var plugin = services[keys[i++]];
-                if (!plugin)
-                    return done();
-                
-                if (plugin.install)
-                    plugin.install(next, progress);
-                else next();
-            }
+            // @todo: @fjakobs do your thing here
+            progress("Installing Nak...");
+            progress("Hello ", true);
+            progress("World.", true);
+            progress("Done.");
+            progress("Installing TMUX...");
+            progress("Goodbye ", true);
+            progress("Cruel ", true);
+            progress("World.", true);
+            progress("Done.");
+            
+            done();
             
             function progress(message, output, error){
                 if (!message.trim()) return;
@@ -145,8 +140,8 @@ define(function(require, exports, module) {
                         log("<div class='output'></div>");
                         lastOutput = logDiv.lastChild;
                     }
-                    // if (error)
-                    //     message = "<span class='error'>" + message + "</span>";
+                    if (error)
+                        errors.push(error);
                     log(message);
                 }
                 else {
@@ -159,10 +154,7 @@ define(function(require, exports, module) {
                 logDiv.style.paddingBottom = "60px";
                 logDiv.scrollTop = logDiv.scrollHeight;
                 
-                button.onclick = function(){
-                    plugin.unload();
-                    callback();
-                };
+                plugin.showCancel = false;
                 
                 if (errors.length) {
                     logln("<span class='error'>"
@@ -173,29 +165,27 @@ define(function(require, exports, module) {
                       + (errors.length == 1 ? "it" : "them")
                       + " and restart cloud9 or contact support@c9.io.</span>");
                       
-                    button.style.display = "block";
                     spinner.style.display = "none";
+                    
+                    plugin.update([
+                        { id: "previous", visible: true },
+                    ]);
                 }
                 else {
-                    fs.writeFile("~/.c9/installed", "1", function(){
-                        logln("Done.");
-                        button.style.display = "block";
-                        spinner.style.display = "none";
-                    });
+                    logln("Done.");
+                    spinner.style.display = "none";
+                    
+                    plugin.showFinish = true;
                 }
             }
-            
-            // Start Installation
-            logln("Starting Installation...");
-            next();
         }
         
         /***** Lifecycle *****/
         
-        plugin.on("install", function(e){
-            install(e.next, e.progress);
-            return false;
+        plugin.on("draw", function(){
+            draw();
         });
+        
         plugin.on("load", function(){
             load();
         });
