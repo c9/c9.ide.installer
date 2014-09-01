ce3f5641bbb6488f04006a7e76fbef747123d273
Lennart Kats <lclkats@gmail.com>
2014-09-01 10:26:35 +0200

    Revert "Merge pull request +4132 from c9/fix/desktop"
    This reverts commit bb9c2591e39afc6fec69283ae54d2b97bdf3345a, reversing
    changes made to 53ac7def4e0e9173aefd7887e3b87ac17d3a1144.

--- installer_mock.js
+++ installer_mock.js
@@ -38,7 +38,7 @@ define(function(require, exports, module) {
          * Installer for Cloud9
          **/
         plugin.freezePublicAPI({
-            show: function(){ alert("installer mock show called"); }
+            show: function(){}
         });
         
         register(null, {
