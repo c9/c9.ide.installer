91b3cc12f8092a3afd2c2cc08dc7fe725d5a82f5
nightwing <amirjanyan@gmail.com>
2015-05-03 20:28:09 +0400

    fix installer on windows

--- install/install.js
+++ install/install.js
@@ -9,53 +9,63 @@ module.exports = function(session, options){
     var nodeName = "node-" + NODEVERSION + "-" 
         + options.platform + "-" + options.arch;
     
-    session.install({
-        "name": "Node.js", 
-        "description": "Node.js " + NODEVERSION
-    }, [
-        {
-            "tar.gz": { 
-                "url": "http://nodejs.org/dist/" + NODEVERSION + "/" + nodeName + ".tar.gz",
-                "target": "~/.c9/node",
-                "dir": nodeName
+    if (options.platform == "win32") {
+        session.preInstallScript = null;
+        session.install({
+            "name": ".c9",
+            "cwd": "~"
+        }, {
+            "bash": "mkdir -p ~/.c9"
+        });
+    }
+    else {
+        session.install({
+            "name": "Node.js", 
+            "description": "Node.js " + NODEVERSION
+        }, [
+            {
+                "tar.gz": { 
+                    "url": "http://nodejs.org/dist/" + NODEVERSION + "/" + nodeName + ".tar.gz",
+                    "target": "~/.c9/node",
+                    "dir": nodeName
+                }
+            },
+            {
+                "bash": require("text!./node.sh")
             }
-        },
-        {
-            "bash": require("text!./node.sh")
-        }
-    ]);
-
-    // Pty.js
-    session.install({
-        "name": "Pty.js",
-        "description": "Pseudo Terminal support. Used by the Cloud9 Terminal",
-        "cwd": "~/.c9"
-    }, {
-        "npm": ["node-gyp", "pty.js@0.2.7-1"]
-    });
+        ]);
     
-    // Tmux
-    session.install({
-        "name": "tmux", 
-        "description": "Tmux - the terminal multiplexer",
-        "cwd": "~/.c9"
-    }, {
-        // // TODO this causes `sudo: no tty present and no askpass program specified` errors
-        // // and it somehow breaks "bash" install method too, also this needs to ensure
-        // // apt-get installs the correct version instead of compiling 1.9 after installing 1.6
-        // "install": [
-        //     {
-        //         "ubuntu": "tmux",
-        //         "centos": "tmux",
-        //         "brew": "tmux"
-        //     },
-        //     {
-        //         "bash": require("text!./tmux.sh")
-        //     }
-        // ],
-        "bash": require("text!./tmux.sh")
-    });
-
+        // Pty.js
+        session.install({
+            "name": "Pty.js",
+            "description": "Pseudo Terminal support. Used by the Cloud9 Terminal",
+            "cwd": "~/.c9"
+        }, {
+            "npm": ["node-gyp", "pty.js@0.2.7-1"]
+        });
+        
+        // Tmux
+        session.install({
+            "name": "tmux", 
+            "description": "Tmux - the terminal multiplexer",
+            "cwd": "~/.c9"
+        }, {
+            // // TODO this causes `sudo: no tty present and no askpass program specified` errors
+            // // and it somehow breaks "bash" install method too, also this needs to ensure
+            // // apt-get installs the correct version instead of compiling 1.9 after installing 1.6
+            // "install": [
+            //     {
+            //         "ubuntu": "tmux",
+            //         "centos": "tmux",
+            //         "brew": "tmux"
+            //     },
+            //     {
+            //         "bash": require("text!./tmux.sh")
+            //     }
+            // ],
+            "bash": require("text!./tmux.sh")
+        });
+    }
     // Show the installation screen
     session.start();
 };
